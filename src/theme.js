b619bb43a7e8b7f42c07fd9b501a774830296fde
Melidorado <meliidorado@hotmail.com>
2021-05-03 23:27:57 -0300

    colors and sizes in theme

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+export const theme = () => {
+    colours:{
+        primary: "#0f171e";
+        secondary:"#1b2530";
+        fontcolor:"#fff"
+    }
+    fontsize:{
+        navlink:{
+            size:"17px";
+        }
+        category:{
+            size:"19px";
+            weight: "700";
+        }
+        titleInCategory:{
+            size:"16px";
+            weight: "300";
+        }
+        titleInSpecific:{
+            size: "25px";
+            weight: "300";
+        }
+        descriptionInSpecific: {
+            size: "14px";
+            weight: "300";
+        }
+    }
+}
